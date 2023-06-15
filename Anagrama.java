38f12af96b1ec501738298d0f2000c8629656017
brayanmega19 <a348572@uach.mx>
2023-06-15 01:36:18 -0700

    Creando el archivo anagrama en el repositorio

---
+++ Anagrama.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+public class Anagrama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programando un anagrama");
+	}
+
+}
