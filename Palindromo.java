db33d110c718e90278a5e9d095e8c27d3f9a08fc
brayanmega19 <a348572@uach.mx>
2023-06-15 16:56:33 -0700

    Programa palindromo terminado

--- Palindromo.java
+++ Palindromo.java
@@ -1,10 +1,29 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Palindromo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		Scanner entry = new Scanner(System.in);
+		System.out.println("Programando un palindromo");
+		
+		System.out.print("Introduce la palabra que se va a analizar si es un palindromo:");
+		String palabra = entry.nextLine();
+		String palabraInvertida = palindromo(palabra);
+		System.out.println(palabraInvertida);
+		if(palabra.equals(palabraInvertida)) System.out.println("Es un palindromo");
+		else System.out.println("No es un palindromo");
+		
+		
+	}
+	public static String palindromo(String palabra) {
+		String palabraInvertida = "";
+		for(int i=palabra.length()-1; i>=0; i--) {
+				palabraInvertida += palabra.charAt(i);
+		}
+		return palabraInvertida;
 	}
 
 }
